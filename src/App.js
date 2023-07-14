7d379fa46475d4f10f1c5b8ee37ca4eeacdc15db
lynne-choi <lynnechoi.dev@gmail.com>
2023-07-14 14:09:40 +0900

    feat: Header 컴포넌트 완성 [#3]

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Main from './page/Main';
+import './base.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Main></Main>
+        첫번째 솔로 프로젝트
+      </header>
+    </div>
+  );
+}
+
+export default App;
